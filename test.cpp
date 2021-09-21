c8e307b197f5bf81e91a790aaf4f73c54808688f
Trevor Green <58746193+trgr5899@users.noreply.github.com>
2021-09-21 13:41:28 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -13,3 +13,8 @@ TEST_CASE("Sign is computing correctly","[Sign]")
   REQUIRE(Sign(-5)==-1);
   REQUIRE(Sign(-33)==-1);
 }
+
+TEST_CASE("Sum all of the numbers in the vector","[Sum]")
+{
+  std::vector<int> v{1,2,3,4,5,6,7,8,9}
+}
